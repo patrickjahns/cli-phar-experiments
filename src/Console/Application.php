6d95e31d7c14a96f6d808f3673dd2a545570dca0
Patrick Jahns <github@patrickjahns.de>
2018-06-24 00:40:41 +0200

    bump version to 0.0.6

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -30,7 +30,7 @@ use Symfony\Component\DependencyInjection\TaggedContainerInterface;
 
 class Application extends BaseApplication
 {
-    const VERSION = '0.0.5';
+    const VERSION = '0.0.6';
 
     /** @var TaggedContainerInterface */
     private $container;
